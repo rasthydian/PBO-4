4160b88f85ba91e80516781bed0aec0ccc58440f
Rasthy <156677015+rasthydian@users.noreply.github.com>
2024-02-20 14:06:09 +0700

    Interface

---
+++ phone/Xiaomi.java
@@ -0,0 +1,61 @@
+
+package com.mycompany.phone;
+
+
+public class Xiaomi implements Phone{
+    private int volume;
+    private boolean isPowerOn;
+
+    public Xiaomi() {
+        // set volume awal
+        this.volume = 50;
+    }
+
+    
+    public void powerOn() {
+        isPowerOn = true;
+        System.out.println("Handphone menyala...");
+        System.out.println("Selamat datang di XIAOMI");
+        System.out.println("Android version 29");
+    }
+
+    
+    public void powerOff() {
+        isPowerOn = false;
+        System.out.println("Handphone dimatikan");
+    }
+
+    
+    public void volumeUp() {
+        if (isPowerOn) {
+            if (this.volume == MAX_VOLUME) {
+                System.out.println("Volume FULL!!");
+                System.out.println("sudah " + this.getVolume() + "%");
+            } else {
+                this.volume += 10;
+                System.out.println("Volume sekarang: " + this.getVolume());
+            }
+        } else {
+            System.out.println("Nyalakan dulu donk HP-nya!!");
+        }
+    }
+
+    
+    public void volumeDown() {
+        if (isPowerOn) {
+            if (this.volume == MIN_VOLUME) {
+                System.out.println("Volume = 0%");
+            } else {
+                this.volume -= 10;
+                System.out.println("Volume sekarang: " + this.getVolume());
+            }
+        } else {
+            System.out.println("Nyalakan dulu donk HP-nya!!");
+        }
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+}
